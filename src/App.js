0a7f9a239f7c7c2ee4e40ef9b5741f880585004d
Kalyani25pachbhai <kpachbhai2004@gmail.com>
2025-03-30 18:31:24 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,30 @@
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import React, { Component } from 'react';
+import Navbar from './Navbar';
+import News from './News';
+import { BrowserRouter, Routes, Route } from 'react-router';
 
-export default App;
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <BrowserRouter>
+          <Navbar />
+          <Routes>
+            <Route exact path="/"><News key="general" pagesize={5} country="us" category="general" /></Route>
+            <Route exact path="/business"><News key="business" pagesize={5} country = "us" category = "business"/></Route>
+            <Route exact path="/entertainment"><News key="entertainment" pagesize={5} country = "us" category = "entertainment"/></Route>
+            <Route exact path="/general"><News key="general" pagesize={5} country = "us" category = "general"/></Route>
+            <Route exact path="/health"><News key="health" pagesize={5} country = "us" category = "health"/></Route>
+            <Route exact path="/science"><News key="science" pagesize={5} country = "us" category = "science"/></Route>
+            <Route exact path="/sports"><News key="sports" pagesize={5} country = "us" category = "general"/></Route>
+            <Route exact path="/technology"><News key="technology" pagesize={5} country = "us" category = "technology"/></Route>
+
+          </Routes>
+        </BrowserRouter>
+      </div>
+    );
+  }
+}
